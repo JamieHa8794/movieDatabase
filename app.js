bbfbeb7452078551f6083faca127cec266118945
JamieHa8794 <JamieHa8794@gmail.com>
2022-06-23 22:58:39 -0400

    express setup

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-console.log('Hello!')
-console.log('Hi')
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`)
+})
+
+
+app.get('/', (req, res, next)=>{
+    const html = `
+    <html>
+        <head>
+        <h1>
+            Movies
+        </h1>
+        </head>
+        <body>
+        </body>
+    </html>
+    `
+    res.send(html)
+})
